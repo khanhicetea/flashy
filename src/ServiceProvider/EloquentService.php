3e46a83586652b0a1ddd76e706a60029abb11981
KhanhIceTea <khanhicetea@gmail.com>
2017-06-07 15:49:21 +0700

    Added EloquentService

---
+++ src/ServiceProvider/EloquentService.php
@@ -0,0 +1,37 @@
+<?php
+namespace Flashy\ServiceProvider;
+
+use Flashy\ServiceProviderInterface;
+use DI\ContainerBuilder;
+use function DI\object;
+use function DI\get;
+use Illuminate\Database\Capsule\Manager;
+use Psr\Container\ContainerInterface;
+
+class EloquentService implements ServiceProviderInterface {
+    public function register(ContainerBuilder $builder, array $opts = []) : void {
+        $def = array_merge([
+            'db.connection' => [
+                'driver' => 'mysql',
+                'host' => 'localhost',
+                'database' => null,
+                'username' => 'root',
+                'password' => null,
+                'charset' => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix' => null,
+            ],
+        ], $opts);
+
+        $def['capsule'] = get(Manager::class);
+        $def['db'] = function(ContainerInterface $c) {
+            return $c['capsule']->getDatabaseManager();
+        };
+        $def[Manager::class] = object(Manager::class)
+            ->method('addConnection', get('db.connection'))
+            ->method('bootEloquent')
+            ->method('setAsGlobal');
+
+        $builder->addDefinitions($def);
+    }
+}
